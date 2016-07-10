5d3d42faf050a19a31d04fb6967620080a2c9b7b
mahendiran43 <kmahee000@gmail.com>
2016-07-10 14:59:13 +0530

    finish

---
+++ README.c
@@ -0,0 +1,38 @@
+# bb
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	void main() {
+	  char text[100];
+	  printf("Please enter strings:");
+
+	   gets(text);
+
+	  while (strlen(text) >= 1){
+
+
+
+
+
+	  if(strchr(text,"bbb")!= NULL)
+
+
+
+	   printf("The string is : %s\n",text);
+
+
+
+
+
+
+
+	     gets(text);
+
+
+
+	  }
+
+
+
+	}
